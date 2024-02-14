f3835bce740e1aaa990313ca53a0975d52c26402
Lutti Davide <luttidavide004@gmail.com>
2024-02-14 15:16:26 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+        OffsetDateTime odt = OffsetDateTime.parse("2004-02-26T13:00:00Z");
+
+        DateTimeFormatter fullFormat = DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL).withLocale(Locale.ITALY);
+
+        DateTimeFormatter mediumFormat = DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM).withLocale(Locale.ITALY);
+
+        DateTimeFormatter shortFormat = DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT).withLocale(Locale.ITALY);
+
+        System.out.println("FULL : " + odt.format(fullFormat));
+        System.out.println("MEDIUM : " + odt.format(mediumFormat));
+        System.out.println("SHORT : " + odt.format(shortFormat));
+    }
+}
